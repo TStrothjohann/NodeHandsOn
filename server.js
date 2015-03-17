90b78d19efd0d47ee9eb0fc41ee9068034cb30b7
Thomas Strothjohann <thomas@strothjohann.net>
2015-03-17 19:21:01 +0000

    page 2 added

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ server.get('/', function(request, response){
   response.send('<h1>Hello and Welcome on my Express server</h1>')
 });
 
+server.get('/page2', function(request, response){
+  response.send("This is page 2")
+});
+
 http.listen(3000, function(){
   console.log('Listening on port 3000')
 });
