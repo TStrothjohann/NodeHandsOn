16278de84f1de72334b209d4004143e28b3a496b
Thomas Strothjohann <thomas@strothjohann.net>
2015-03-17 19:30:00 +0000

    Added ejs and page 3

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 var server = require('express')();
 var http = require('http').createServer(server);
 
+server.set('views', __dirname + '/views');
+server.set('view engine', 'ejs');
+
 server.get('/', function(request, response){
   response.send('<h1>Hello and Welcome on my Express server</h1>')
 });
 
 server.get('/page2', function(request, response){
   response.send("This is page 2")
+});
+
+server.get('/page3', function(request, response) {
+  response.render('index')
 });
 
 http.listen(3000, function(){
