f7172beda46e0f49059b5945352f9eb7ed1d1d34
Thomas Strothjohann <thomas@strothjohann.net>
2015-03-18 08:51:16 +0000

    Added a little chat based on socket.io

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 var server = require('express')();
 var http = require('http').createServer(server);
+var io = require('socket.io')(http);
+
 
 server.set('views', __dirname + '/views');
 server.set('view engine', 'ejs');
 
+
 server.get('/', function(request, response){
-  response.send('<h1>Hello and Welcome on my Express server</h1>')
+  response.render('index')
+});
+
+server.get('/chat', function(request, response){
+  response.sendFile(__dirname + '/websockets.html')
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
 server.get('/page2', function(request, response){
   response.send("This is page 2")
-});
-
-server.get('/page3', function(request, response) {
-  response.render('index')
 });
 
 http.listen(3000, function(){
